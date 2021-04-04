6776fd3985bb0cc5f51f23fe54425a2867b2fa66
Bryan-Murl <murillo.bryan@outlook.com>
2021-04-04 07:34:39 +0000

    IG model re-final

--- src/models.py
+++ src/models.py
@@ -8,13 +8,14 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(250))
-    firstname = Column(String(250))
-    lastname = Column(String(250))
-    email = Column(String(250))
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
 class Post(Base):
     __tablename__ = 'post'
@@ -24,7 +25,7 @@ class Post(Base):
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    url = Column(String(250))
+    url = Column(String(250), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'))
 
 class Follow(Base):
@@ -36,7 +37,7 @@ class Follow(Base):
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
+    comment_text = Column(String(250), nullable=False)
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
 
